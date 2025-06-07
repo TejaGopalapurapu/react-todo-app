1fbe25f7b3cb556b5f72b14864eca7811b33b642
TejaGopalapurapu <gopalapuraputeja@gmail.com>
2025-06-08 01:53:05 +0530

    todo1

--- src/Pages/Completed.js
+++ src/Pages/Completed.js
@@ -4,7 +4,7 @@ function Completed() {
   return (
     <div style={{ textAlign: "center" }}>
       <h2>Completed Todos</h2>
-      <h2>Todos Completed</h2>
+      <h2>Todos Completed file</h2>
       <p>Here we will show completed todos.</p>
     </div>
   );
